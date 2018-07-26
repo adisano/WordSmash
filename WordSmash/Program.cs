feed38a39a2ce4e45c3a828ec8e06d9b4ba9fb24
Adriana Disano <adrianadisano@gmail.com>
2018-07-25 21:02:23 -0500

    Updated, buggy

--- WordSmash/Program.cs
+++ WordSmash/Program.cs
@@ -6,7 +6,50 @@ namespace WordSmash
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!");
+            Console.WriteLine("This application will take two words and make them into one word.");
+            Console.WriteLine("Please enter the first word:");
+            string wordFirst = Console.ReadLine();
+            Console.WriteLine("Please enter the second word:");
+            string wordSecond = Console.ReadLine();
+
+            char[] arrayFirst = wordFirst.ToCharArray();
+            char[] arraySecond = wordSecond.ToCharArray();
+
+            int arrayFirstHalf = 0;
+            int arraySecondHalf = 0;
+
+            if (arrayFirst.Length == 3 && arraySecond.Length == 3)
+            {
+                arrayFirstHalf = arrayFirst.Length - 1;
+                arraySecondHalf = arraySecond.Length - 2;
+            }
+            else if (arrayFirst.Length == 3)
+            {
+                arrayFirstHalf = arrayFirst.Length - 1;
+            }
+            else if (arraySecond.Length == 3)
+            {
+                arraySecondHalf = arraySecond.Length - 1;
+            }
+            else
+            {
+                arrayFirstHalf = arrayFirst.Length / 2;
+                arraySecondHalf = arraySecond.Length / 2;
+            }
+
+            Console.WriteLine("Smashed word:");
+
+            for (int loop = 0; loop < arrayFirstHalf; loop++)
+            {
+                Console.Write(arrayFirst[loop]);
+            }
+
+            for (int loop = arraySecondHalf; loop < (arraySecondHalf + 1); loop++)
+            {
+                Console.Write(arraySecond[loop]);
+            }
+
+            Console.ReadLine();
         }
     }
 }
